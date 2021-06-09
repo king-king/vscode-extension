e0fb361866594c1922426636d750c650d009f6e1
wangqun6 <wangqun28>
2021-06-09 15:03:24 +0800

    add vscode-theme-onedark-master

--- packages/helloworld/extension.js
+++ packages/helloworld/extension.js
@@ -1,27 +1,19 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-
-/**
- * @param {vscode.ExtensionContext} context
- */
 function activate(context) {
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "helloworld" is now active!');
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('helloworld.helloWorld', function () {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World from HelloWorld!');
-    });
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(
+        vscode.languages.registerHoverProvider("javascript", {
+            provideHover: (document, position) => {
+                const commentCommandUri = vscode.Uri.parse(`command:editor.action.addCommentLine`);
+                const contents = new vscode.MarkdownString(`[Add comment](${commentCommandUri})`);
+                // To enable command URIs in Markdown content, you must set the `isTrusted` flag.
+                // When creating trusted Markdown string, make sure to properly sanitize all the
+                // input content so that only expected command URIs can be executed
+                contents.isTrusted = true;
+                return new vscode.Hover(contents);
+            },
+        })
+    );
 }
 
 
