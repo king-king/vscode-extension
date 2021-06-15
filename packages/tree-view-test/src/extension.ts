fc199d248566e6b43e11b8b9a4c831ff0ccfbacb
wangqun6 <wangqun28>
2021-06-15 16:30:05 +0800

    增加视图按钮

--- packages/tree-view-test/src/extension.ts
+++ packages/tree-view-test/src/extension.ts
@@ -4,7 +4,11 @@ import { NodeDependenciesProvider, Dependency } from './nodeDependencies';
 export function activate(context: vscode.ExtensionContext) {
     const nodeDependenciesProvider = new NodeDependenciesProvider(vscode.workspace.rootPath);
     vscode.window.registerTreeDataProvider('nodeDependencies', nodeDependenciesProvider);
+
     vscode.commands.registerCommand('nodeDependencies.refreshEntry', () =>
         nodeDependenciesProvider.refresh()
     );
+    vscode.commands.registerCommand('nodeDependencies.addEntry', () => vscode.window.showInformationMessage(`Successfully called add entry.`));
+    vscode.commands.registerCommand('nodeDependencies.editDep', (node: Dependency) => vscode.window.showInformationMessage(`Successfully called edit entry on ${node.label}.`));
+    vscode.commands.registerCommand('nodeDependencies.deleteDep', (node: Dependency) => vscode.window.showInformationMessage(`Successfully called delete entry on ${node.label}.`));
 }
